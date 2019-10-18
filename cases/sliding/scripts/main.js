8b0358d58e10f7e81246c8f11525a13df4b617d2
gitguanqi <jcguanqi@126.com>
2019-10-18 21:10:18 +0800

    dev

---
+++ cases/sliding/scripts/main.js
@@ -0,0 +1,51 @@
+/*
+ * @Author: guanqi 
+ * @Date: 2019-05-11 16:10:10 
+ * @Last Modified by: GuanQi
+ * @Last Modified time: 2019-06-07 09:33:52
+ */
+
+//成功以后访问的地址
+let succFn = function () {
+  setTimeout(function () {
+    let toUrl = 'https://github.com/gitguanqi';
+    window.location.href = toUrl;
+  }, 1000)
+}
+// 验证图片库
+let picArrs = [
+  '../sliding/images/v1.jpg',
+  '../sliding/images/v2.jpg',
+  '../sliding/images/v3.jpg',
+  '../sliding/images/v4.jpg',
+  '../sliding/images/v5.jpg',
+  '../sliding/images/v6.jpg',
+  '../sliding/images/v7.jpg',
+  '../sliding/images/v8.jpg'
+]
+let pic = document.querySelector('#pic');
+let msg = document.querySelector('#msg');
+if (window.SlidingValidate) {
+  window.SlidingValidate.init({
+    el: pic,
+    //验证成功
+    onSuccess: function () {
+      succFn()
+    },
+    //验证失败
+    onFail: function () {
+      msg.innerHTML = '';
+    },
+    imgLibs: picArrs
+  })
+}
+//获取验证码
+console.log('验证码图片:', getRandomImgSrc());
+
+function getRandomImgSrc() {
+  return '//picsum.photos/300/150/?image=' + getRandomNumberByRange(0, 1084)
+}
+
+function getRandomNumberByRange(start, end) {
+  return Math.round(Math.random() * (end - start) + start)
+}
---
+++ public/scripts/main.js
@@ -0,0 +1,44 @@
+/*
+ * @Author: fed_guanqi 
+ * @Date: 2019-10-18 19:49:27 
+ * @Last Modified by: fed_guanqi
+ * @Last Modified time: 2019-10-18 21:05:03
+ */
+let siteTitle = document.querySelector('.site-title');
+let counts = document.querySelectorAll('.count');
+let contentItem = document.querySelectorAll('.demo-content-item-ls');
+
+getData();
+async function getData () {
+    let data = await axios.get('./public/mock/list.json');
+    document.title = data.data.name + '- 探索前端新技术';
+    siteTitle.innerText = data.data.name;
+    let list = data.data.data.v1.data.list;
+    showData(list);
+}
+
+function showData (list) {
+    let contentItem = document.querySelectorAll('.demo-content-item-ls');
+    let host = location.origin;
+    list.forEach(element => {
+        let cIndex = element.cid.toString().split('')[0] - 1;
+        let tags = element.tags.split(',').join(', ');
+        if (element.href.indexOf('http') > -1 || element.href.indexOf('https') > -1) {
+            element.href = host + '/link/?url=' + element.href;
+        } else {
+            element.href = host + '/' + element.href;
+        }
+        contentItem[cIndex].innerHTML += `<li>
+            <a href="${element.href}" target="_blank" title="${element.description}">
+                <img src="${host}/./public/images/holder.png" data-src="${host + '/' + element.picUrl}" alt="${element.name}">
+                <span class="project-title">${element.name}</span>
+                <span class="project-des">${element.description}</span>
+                <span class="project-tags"><i class="fa fa-tags"></i> ${tags}</span>
+            </a>
+        </li>`;
+    });
+    for (let i = 0; i < contentItem.length; i++) {
+        const element = contentItem[i];
+        counts[i].innerText = element.childNodes.length;
+    }
+}
