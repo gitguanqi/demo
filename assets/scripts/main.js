48fac9b97ce42d5e6236ec69dc714ebfa81edaea
gitguanqi <jcguanqi@126.com>
2019-10-19 11:21:12 +0800

    dev

---
+++ assets/scripts/main.js
@@ -0,0 +1,124 @@
+/*
+ * @Author: fed_guanqi 
+ * @Date: 2019-10-18 19:49:27 
+ * @Last Modified by: fed_guanqi
+ * @Last Modified time: 2019-10-19 11:09:10
+ */
+let siteTitle = document.querySelector('.site-title');
+let counts = document.querySelectorAll('.count');
+let content = document.querySelector('.demo-content');
+let navLis = document.querySelectorAll('.demo-nav-content li a');
+let contents = document.querySelectorAll('.demo-content-item');
+let contentItem = document.querySelectorAll('.demo-content-item-ls');
+let listUrl = '../assets/mock/list.json';
+// 到顶部
+let goTopBtn = document.querySelector('.demo-go-top');
+
+// 获取数据
+getData();
+async function getData () {
+    let data = await axios.get(listUrl);
+    document.title = data.data.name + '- 探索前端新技术';
+    siteTitle.innerText = data.data.name;
+    let list = data.data.data.v1.data.list;
+    showData(list);
+}
+
+// 显示数据
+function showData (list) {
+    let contentItem = document.querySelectorAll('.demo-content-item-ls');
+    let host = location.origin;
+    list.forEach(element => {
+        let cIndex = element.cid.toString().split('')[0] - 1;
+        let tags = element.tags.split(',').join(', ');
+        if (element.href.indexOf('http') > -1 || element.href.indexOf('https') > -1) {
+            element.href = host + '/link/?url=' + element.href;
+        } else {
+            element.href = host + '/' + element.href;
+        }
+        contentItem[cIndex].innerHTML += `<li>
+            <a href="${element.href}" target="_blank" title="${element.description}">
+                <img class="lazyimg" src="./assets/images/holder.png" data-src="${'./' + element.picUrl}" alt="${element.name}">
+                <span class="project-title">${element.name}</span>
+                <span class="project-des">${element.description}</span>
+                <span class="project-tags"><i class="fa fa-tags"></i> ${tags}</span>
+            </a>
+        </li>`;
+    });
+    for (let i = 0; i < contentItem.length; i++) {
+        const element = contentItem[i];
+        counts[i].innerText = element.childNodes.length;
+    }
+    lazyLoad();
+}
+
+// 图片懒加载
+function lazyLoad() {
+    let n = 0;
+    let lazyImgs = document.querySelectorAll('.lazyimg');
+    let clientHeight = document.documentElement.clientHeight || document.body.clientHeight;
+    let scrollTop = document.documentElement.scrollTop || document.body.scrollTop;
+    for (var i = n; i < lazyImgs.length; i++) {
+      if (lazyImgs[i].offsetTop < (clientHeight + scrollTop)) {
+        let secImgs = lazyImgs[i];
+        if (secImgs.getAttribute('src') === './assets/images/holder.png') {
+            secImgs.src = secImgs.getAttribute('data-src');
+        }
+      }
+      n = i + 1;
+    }
+}
+
+window.addEventListener('scroll', function(){
+    // 懒加载
+    setTimeout(() => {
+        lazyLoad();
+    }, 500);
+    // 到顶部
+    let scrollTop = document.documentElement.scrollTop || document.body.scrollTop;
+    if (scrollTop > 280) {
+        goTopBtn.style.display = 'block';
+    } else {
+        goTopBtn.style.display = 'none';
+    }
+    // 滚动高亮
+    for (let i = 0; i < contents.length; i++) {
+        const element = contents[i].offsetTop - 50;
+        if (element <= scrollTop) {
+          for (let j = 0; j < navLis.length; j++) {
+            navLis[j].classList = '';
+          }
+          navLis[i].classList = 'active';
+        }
+      }
+}, false);
+
+// 点击到对应
+goNavContent();
+function goNavContent () {
+    for (let i = 0; i < navLis.length; i++) {
+        const element = navLis[i];
+        element.addEventListener('click', function(e) {
+            e.preventDefault();
+            let contentTop = contents[i].offsetTop;
+            document.documentElement.scrollTop = contentTop;
+            document.body.scrollTop = contentTop;
+        })
+    }
+}
+
+// 到顶部
+$g.addEvent(goTopBtn, 'click', goTop, false);
+function goTop () {
+    let timer = setInterval(function () {
+    let top = document.documentElement.scrollTop || document.body.scrollTop;
+    let speed = 0;
+    if (top > 0) {
+        speed = Math.floor(-top / 6);
+    }
+    if (top == 0) {
+        clearInterval(timer);
+    }
+    document.documentElement.scrollTop = document.body.scrollTop = top + speed;
+    }, 10);
+}
