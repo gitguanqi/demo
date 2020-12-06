8d2a346a31ba199305cd9697bd255c4d79ce3aa5
gitguanqi <jcguanqi@126.com>
2020-12-06 14:48:27 +0800

    add js[D[Dhook.js

---
+++ assets/mock/webhook.js
@@ -0,0 +1,8 @@
+/*
+ * @Author: MarkGuan
+ * @LastEditors: MarkGuan
+ * @Date: 2020-12-06 14:47:50
+ * @LastEditTime: 2020-12-06 14:48:04
+ * @Description: This is web code commit.
+ */
+console.log('deploy success!');
