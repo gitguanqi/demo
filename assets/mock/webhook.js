3b25c507a399d1db2b8706381ea3a4b65e49f4c2
gitguanqi <jcguanqi@126.com>
2020-12-06 16:12:17 +0800

    test hook

--- assets/mock/webhook.js
+++ assets/mock/webhook.js
@@ -2,10 +2,9 @@
  * @Author: MarkGuan
  * @LastEditors: MarkGuan
  * @Date: 2020-12-06 14:47:50
- * @LastEditTime: 2020-12-06 16:09:18
+ * @LastEditTime: 2020-12-06 16:12:06
  * @Description: This is web code commit.
  */
-console.log('deploy success!111');
 console.log('deploy success!111');
 console.log('deploy success!111');
 console.log('deploy success!111');
