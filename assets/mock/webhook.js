7fd61df1e9ffe9504b12827173371755a255f56f
gitguanqi <jcguanqi@126.com>
2020-12-06 14:57:44 +0800

    fix gook

--- assets/mock/webhook.js
+++ assets/mock/webhook.js
@@ -2,7 +2,7 @@
  * @Author: MarkGuan
  * @LastEditors: MarkGuan
  * @Date: 2020-12-06 14:47:50
- * @LastEditTime: 2020-12-06 14:48:04
+ * @LastEditTime: 2020-12-06 14:57:30
  * @Description: This is web code commit.
  */
-console.log('deploy success!');
+console.log('deploy success!111');
