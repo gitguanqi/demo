7fbcb4141f55a2c69a28de198fc0ec8efc85b0ac
gitguanqi <jcguanqi@126.com>
2020-12-08 21:33:31 +0800

    fix matomo

--- assets/scripts/matomo.js
+++ assets/scripts/matomo.js
@@ -2,14 +2,11 @@
  * @Author: MarkGuan
  * @LastEditors: MarkGuan
  * @Date: 2020-12-08 21:14:03
- * @LastEditTime: 2020-12-08 21:15:32
+ * @LastEditTime: 2020-12-08 21:32:56
  * @Description: This is web code commit.
  */
 var _paq = window._paq = window._paq || [];
 /* tracker methods like "setCustomDimension" should be called before "trackPageView" */
-_paq.push(["setDocumentTitle", document.domain + "/" + document.title]);
-_paq.push(["setCookieDomain", "*.guanqi.xyz"]);
-_paq.push(["setDomains", ["*.guanqi.xyz/fe"]]);
 _paq.push(['trackPageView']);
 _paq.push(['enableLinkTracking']);
 (function () {
