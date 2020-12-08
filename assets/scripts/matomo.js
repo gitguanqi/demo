a6f94c04e69ac7f6e58bc841f42c40b508df3033
gitguanqi <jcguanqi@126.com>
2020-12-08 21:19:26 +0800

    add matomo

---
+++ assets/scripts/matomo.js
@@ -0,0 +1,26 @@
+/*
+ * @Author: MarkGuan
+ * @LastEditors: MarkGuan
+ * @Date: 2020-12-08 21:14:03
+ * @LastEditTime: 2020-12-08 21:15:32
+ * @Description: This is web code commit.
+ */
+var _paq = window._paq = window._paq || [];
+/* tracker methods like "setCustomDimension" should be called before "trackPageView" */
+_paq.push(["setDocumentTitle", document.domain + "/" + document.title]);
+_paq.push(["setCookieDomain", "*.guanqi.xyz"]);
+_paq.push(["setDomains", ["*.guanqi.xyz/fe"]]);
+_paq.push(['trackPageView']);
+_paq.push(['enableLinkTracking']);
+(function () {
+    var u = "//am.guanqi.xyz/";
+    _paq.push(['setTrackerUrl', u + 'matomo.php']);
+    _paq.push(['setSiteId', '2']);
+    var d = document,
+        g = d.createElement('script'),
+        s = d.getElementsByTagName('script')[0];
+    g.type = 'text/javascript';
+    g.async = true;
+    g.src = u + 'matomo.js';
+    s.parentNode.insertBefore(g, s);
+})();
